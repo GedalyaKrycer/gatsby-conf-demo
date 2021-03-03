00c38aa0c039a8b750c7bf01aac0e84ad18a30e1
Gedalya Krycer <gedalya@krycer.com>
2021-03-03 12:42:24 -0800

    Add Blog

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import {navBar} from './layout.module.css';
 
 const Layout = ({children, pageTitle}) => {
+
+    const data = useStaticQuery(graphql`
+        query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+    `)
+
     return (
         <main>
-            <title>{pageTitle}</title>
-            <h1>
-                {pageTitle}
-            </h1>
+            <p>{data.site.siteMetadata.title}</p>
             <nav>
                 <ul>
                     <li className={navBar}><Link to="/">Home</Link></li>
                     <li className={navBar}><Link to="/about">About</Link></li>
+                    <li className={navBar}><Link to="/blog">Blog</Link></li>
                 </ul>
             </nav>
+            <title>{pageTitle}</title>
+            <h1>
+                {pageTitle}
+            </h1>
 
         {children}
         </main>
